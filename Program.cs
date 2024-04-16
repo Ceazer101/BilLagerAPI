82375182d81c762142f69dbb262c4c92cad9fdb7
chris <chri402s@stud.kea.dk>
2024-04-16 12:16:55 +0200

    made program.cs into an app and made acces for guest role in DB

--- Program.cs
+++ Program.cs
@@ -1,45 +1,66 @@
 using System;
 using Microsoft.Extensions.Configuration;
-using BilLagerAPI.Services; 
-using BilLagerAPI.Data; 
+using BilLagerAPI.Services;
+using BilLagerAPI.Data;
 using BilLagerAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace BilLagerAPI
+public class Program
 {
-    class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+    {
+        webBuilder.ConfigureServices(services =>
+        {
+            // Add services here
+        })
+        .Configure((context, app) =>
         {
             var builder = new ConfigurationBuilder()
-            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-            .AddJsonFile("config/appsettings.json");
+                .SetBasePath(context.HostingEnvironment.ContentRootPath)
+                .AddJsonFile("config/appsettings.json");
 
             IConfiguration configuration = builder.Build();
 
             DatabaseConnector databaseConnector = new DatabaseConnector(configuration);
             databaseConnector.Connect();
 
-            CarContext context = new CarContext();
+            CarContext carContext = new CarContext();
 
-            CarInventory carInventory = new CarInventory(context);
-            //carInventory.CreateStandardCars();
-            var missingMigrations = context.Database.GetPendingMigrations();
+            CarInventory carInventory = new CarInventory(carContext);
+
+            var missingMigrations = carContext.Database.GetPendingMigrations();
             if (missingMigrations.Any())
-
             {
-
-                context.Database.Migrate();
-
+                carContext.Database.Migrate();
             }
-            List<StandardCar> standardCars = carInventory.GetStandardCars();
-            foreach (StandardCar car in standardCars)
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
             {
-                Console.WriteLine($"Model: {car.Name}, Color: {car.Color}");Console.WriteLine($"Id: {car.Id}, Name: {car.Name}, Type: {car.Type}, Color: {car.Color}, Battery: {car.Battery}, Hitch: {car.Hitch}");
-            }
-            
-            RabbitMQService rabbitMQService = new RabbitMQService("host.docker.internal", "test_queue", "guest", "guest");
-            rabbitMQService.SendMessage("Hello, RabbitMQ!");
-        }
-    }
+                endpoints.MapGet("/", async context =>
+                {
+                    List<StandardCar> standardCars = carInventory.GetStandardCars();
+                    foreach (StandardCar car in standardCars)
+                    {
+                        await context.Response.WriteAsync($"Model: {car.Name}, Color: {car.Color}\n");
+                        await context.Response.WriteAsync($"Id: {car.Id}, Name: {car.Name}, Type: {car.Type}, Color: {car.Color}, Battery: {car.Battery}, Hitch: {car.Hitch}\n");
+                    }
+                });
+            });
+        });
+    });
 }
