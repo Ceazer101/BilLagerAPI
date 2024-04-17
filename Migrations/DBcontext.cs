e09ee5bf883761c65faf11f71f1b7a3876fdfb6e
Bjørn <bjand49@gmail.com>
2024-04-17 13:45:45 +0200

    Updated connectionstring for PostGresSQL

--- Migrations/DBcontext.cs
+++ Migrations/DBcontext.cs
@@ -7,10 +7,6 @@ namespace BilLagerAPI.Data
     {
         public DbSet<StandardCar> StandardCars { get; set; }
         public DbSet<CustomCar> CustomCars { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseNpgsql("Host=host.docker.internal;Port=5432;Database=BilLagerDB;Username=guest;Password=guest;");
-        }
+        public CarContext(DbContextOptions options) : base(options) { }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,15 +1,28 @@
+using BilLagerAPI.Controllers;
+using BilLagerAPI.Data;
 using BilLagerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        var configuration = builder.Configuration;
+
         builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddDbContext<CarContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
         builder.Services.AddSwaggerGen();
-
         var app = builder.Build();
-
+        using (var scope = app.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<CarContext>();
+            db.Database.Migrate();
+        }
         app.UseRouting();
         app.MapGet("/", () => "Hello World!");
         app.UseSwagger();
