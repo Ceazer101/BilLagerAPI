953e965c4170e97d769dd0edccc102532cb2c303
Bjørn <bjand49@gmail.com>
2024-04-17 12:27:03 +0200

    Upgraded to .NET 8.0

--- Program.cs
+++ Program.cs
@@ -1,66 +1,20 @@
-using System;
-using Microsoft.Extensions.Configuration;
-using BilLagerAPI.Services;
-using BilLagerAPI.Data;
 using BilLagerAPI.Models;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-public class Program
+internal class Program
 {
-    public static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        CreateHostBuilder(args).Build().Run();
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        app.UseRouting();
+        app.MapGet("/", () => "Hello World!");
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.Run();
     }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-        .ConfigureWebHostDefaults(webBuilder =>
-    {
-        webBuilder.ConfigureServices(services =>
-        {
-            // Add services here
-        })
-        .Configure((context, app) =>
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(context.HostingEnvironment.ContentRootPath)
-                .AddJsonFile("config/appsettings.json");
-
-            IConfiguration configuration = builder.Build();
-
-            DatabaseConnector databaseConnector = new DatabaseConnector(configuration);
-            databaseConnector.Connect();
-
-            CarContext carContext = new CarContext();
-
-            CarInventory carInventory = new CarInventory(carContext);
-
-            var missingMigrations = carContext.Database.GetPendingMigrations();
-            if (missingMigrations.Any())
-            {
-                carContext.Database.Migrate();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    List<StandardCar> standardCars = carInventory.GetStandardCars();
-                    foreach (StandardCar car in standardCars)
-                    {
-                        await context.Response.WriteAsync($"Model: {car.Name}, Color: {car.Color}\n");
-                        await context.Response.WriteAsync($"Id: {car.Id}, Name: {car.Name}, Type: {car.Type}, Color: {car.Color}, Battery: {car.Battery}, Hitch: {car.Hitch}\n");
-                    }
-                });
-            });
-        });
-    });
 }
